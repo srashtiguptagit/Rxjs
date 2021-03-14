0d5722a3f166ca010473f26becd2234fce411674
srashtiguptagit <srashtigupta1708@gmail.com>
2021-03-14 18:41:15 +0530

    first commit

---
+++ Rxjs-Operators/rxjsMapOperator.ts
@@ -0,0 +1,8 @@
+// Suppose we want to use the existing observable and then tranform the data
+
+const http$  = createObservale('hasgdjhgsadjgdas');
+
+const courses = http$.pipe(map((body)=>{
+    console.log(courses);
+
+}))
